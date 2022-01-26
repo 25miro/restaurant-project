28fb58cab758ddd99f5bcb95bf6fcf1c7c1d0864
miro25 <silva.belmiro@gmail.com>
2022-01-26 09:21:25 +0000

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,21 +7,17 @@ export default function Footer() {
     <>
 
     <footer className="footer mb-0">
-
-                <div className="row col-md-12 py-4 d-flex align-items-center">
-                    <div className="col-md-6 col-lg-6 text-center text-md-left mb-4 mb-md-0">
-                        <h6 className="mb-0">Oferta do valor da entrega em compras superiores a 50{"\u20AC"}</h6>
-                    </div>
-                    <div className="col-md-6 col-lg-6 text-center text-md-left mb-4 mb-md-0">
-                        <h6 className="mb-0">React | Javascript | Bootstrap | CSS | Git </h6>
-                    </div>
-                    <div className="footer-copyright text-center py-3"> © 2022 Belmiro Silva </div>
-                </div>
-
-</footer>
+        <div className="row col-md-12 py-4 d-flex align-items-center">
+            <div className="col-md-6 col-lg-6 text-center text-md-left mb-4 mb-md-0">
+                <h6 className="mb-0">Oferta do valor da entrega em compras superiores a 50{"\u20AC"}</h6>
+            </div>
+            <div className="col-md-6 col-lg-6 text-center text-md-left mb-4 mb-md-0">
+                <h6 className="mb-0">React | Javascript | Bootstrap | CSS | Git </h6>
+            </div>
+            <div className="footer-copyright text-center py-3"> © 2022 Belmiro Silva </div>
+        </div>
+    </footer>
 
     </>
-
-
     )
 }
